446dd6f925b508a2273201800ddc4d8220627391
Segun Ajibola <133857312+duongluuba@users.noreply.github.com>
2023-08-26 22:49:48 +0700

    first commit

---
+++ Student.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self, code = '', name = '', old = 0, lab = 0, pt = 0, assignment = 0, pe = 0, fe = 0, avg = 0, rank = ''):
+        self.code = code
+        self.name = name
+        self.old = old 
+        self.lab = lab 
+        self.pt = pt 
+        self.assignment = assignment 
+        self.pe = pe
+        self.fe = fe
+        self.avg = avg
+        self.rank = rank
+    # end def
+# end class
